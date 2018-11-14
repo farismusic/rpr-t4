781e025dad7828152e3fb067fc6063e1767a95a8
farismusic <44246682+farismusic@users.noreply.github.com>
2018-11-14 19:10:20 +0100

    Implementirana klasa predmet

--- rpr-t4/src/ba/unsa/etf/rpr/predavanje03/Predmet.java
+++ rpr-t4/src/ba/unsa/etf/rpr/predavanje03/Predmet.java
@@ -1,34 +1,57 @@
 package ba.unsa.etf.rpr.predavanje03;
 
+import java.util.ArrayList;
+
 public class Predmet {
     private String ime;
     private int iznosECTS;
-    private Student[] upisaniStuddenti;
-    private Semestar semestar;
+    private ArrayList<Student> studenti;
     private String imePrezimeProfesora;
 
     public Predmet(){};
-    public Predmet(String imePredmeta, String profesor, int ects, int semestar){};
-    public void dodajStudenta(Student student){};
-    public void obrisiStudenta(Student student){};
+    public Predmet(String imePredmeta, String profesor, int ects){
+        setIme(imePredmeta);
+        this.setImePrezimeProfesora(profesor);
+        setIznosECTS(ects);
+        setStudenti(new ArrayList<Student>());
+    }
+    public void dodajStudenta(Student student){
+        getStudenti().add(student);
+    }
+    public void obrisiStudenta(Student student){
+        getStudenti().remove(student);
+    }
+
 
     public String getIme() {
         return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
     }
 
     public int getIznosECTS() {
         return iznosECTS;
     }
 
-    public Student[] getUpisaniStuddenti() {
-        return upisaniStuddenti;
+    public void setIznosECTS(int iznosECTS) {
+        this.iznosECTS = iznosECTS;
     }
 
-    public Semestar getSemestar() {
-        return semestar;
+    public ArrayList<Student> getStudenti() {
+        return studenti;
+    }
+
+    public void setStudenti(ArrayList<Student> studenti) {
+        this.studenti = studenti;
     }
 
     public String getImePrezimeProfesora() {
         return imePrezimeProfesora;
+    }
+
+    public void setImePrezimeProfesora(String imePrezimeProfesora) {
+        this.imePrezimeProfesora = imePrezimeProfesora;
     }
 }
