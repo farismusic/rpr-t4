04d8f59dc686d7f20b7ba0d33ca69a79e482ab2f
farismusic <44246682+farismusic@users.noreply.github.com>
2018-11-14 19:17:22 +0100

    Implementirana klasa PlanStudija

--- rpr-t4/src/ba/unsa/etf/rpr/predavanje03/PlanStudija.java
+++ rpr-t4/src/ba/unsa/etf/rpr/predavanje03/PlanStudija.java
@@ -1,4 +1,52 @@
 package ba.unsa.etf.rpr.predavanje03;
 
+import java.util.ArrayList;
+
 public class PlanStudija {
+    private String imeStudija;
+    private ArrayList<Predmet> obavezniPredmeti;
+    private ArrayList<Predmet> izborniPredmeti;
+
+    public PlanStudija(String ime){
+        setImeStudija(ime);
+        setObavezniPredmeti(new ArrayList<Predmet>());
+        setIzborniPredmeti(new ArrayList<Predmet>());
+    }
+
+    public void dodajObavezniPredmet(Predmet predmet){
+        getObavezniPredmeti().add(predmet);
+    }
+    public void dodajIzborniPredmet(Predmet predmet){
+        getIzborniPredmeti().add(predmet);
+    }
+    public void obrisiObavezniPredmet(Predmet predmet){
+        getObavezniPredmeti().remove(predmet);
+    }
+    public void obrisiIzborniPredmet(Predmet predmet){
+        getIzborniPredmeti().remove(predmet);
+    }
+
+    public String getImeStudija() {
+        return imeStudija;
+    }
+
+    public void setImeStudija(String imeStudija) {
+        this.imeStudija = imeStudija;
+    }
+
+    public ArrayList<Predmet> getObavezniPredmeti() {
+        return obavezniPredmeti;
+    }
+
+    public void setObavezniPredmeti(ArrayList<Predmet> obavezniPredmeti) {
+        this.obavezniPredmeti = obavezniPredmeti;
+    }
+
+    public ArrayList<Predmet> getIzborniPredmeti() {
+        return izborniPredmeti;
+    }
+
+    public void setIzborniPredmeti(ArrayList<Predmet> izborniPredmeti) {
+        this.izborniPredmeti = izborniPredmeti;
+    }
 }
